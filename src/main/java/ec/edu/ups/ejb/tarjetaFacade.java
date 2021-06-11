84007a61bc5ae8bd9890621930a120d6fab757ee
andipandiber <andipandi467@gmail.com>
2021-06-11 10:35:13 -0500

    Creation of Facade and Beans

--- src/main/java/ec/edu/ups/ejb/tarjetaFacade.java
+++ src/main/java/ec/edu/ups/ejb/tarjetaFacade.java
@@ -1,10 +1,24 @@
 package ec.edu.ups.ejb;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import ec.edu.ups.entidad.tarjeta;
 
 @Stateless
 public class tarjetaFacade extends AbstractFacade<tarjeta> {
+	
+	@PersistenceContext(unitName = "Jdbc/__Products")
+	private EntityManager entityManager;
+
+	public tarjetaFacade() {
+		super(tarjeta.class);
+	}
+
+	@Override
+	protected EntityManager getEntityManager() {
+		return entityManager;
+	}
 
 }
